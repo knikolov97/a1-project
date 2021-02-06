b2beddc7f8f28f82683dd7de7753956d686f91f5
kirilnikolov <kiril.nikolov.97@gmail.com>
2021-02-06 19:18:05 +0200

    Create footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from "./components/pages/Home";
 import React from "react";
 import LaunchDetails from "./components/pages/LaunchDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-      <div>
+      <div className="d-flex flex-column min-vh-100">
           <Router>
               <Navbar/>
               <Switch>
                   <Route path="/" exact component={Home}/>
                   <Route path={"/launch/:id"} component={LaunchDetails}/>
               </Switch>
+              <Footer/>
           </Router>
       </div>
   );
